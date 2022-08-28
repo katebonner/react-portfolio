943c89f8fa14c348844c9217dec9c2e5ce82372c
Kate Bonner <katebonner277@gmail.com>
2022-08-28 18:57:45 -0400

    Updates

---
+++ precache-manifest.f9d57f00f63af8368fc3ed51f561d4ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "008bb998a481787923eb",
+    "url": "/react-portfolio/static/css/main.bac9de81.chunk.css"
+  },
+  {
+    "revision": "008bb998a481787923eb",
+    "url": "/react-portfolio/static/js/main.008bb998.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/react-portfolio/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "df928d0653a36307852a",
+    "url": "/react-portfolio/static/js/2.df928d06.chunk.js"
+  },
+  {
+    "revision": "31f0d8487f014aa0cc61",
+    "url": "/react-portfolio/static/js/runtime~main.31f0d848.js"
+  },
+  {
+    "revision": "821d293c0284b0af84ca3ab50c1f5259",
+    "url": "/react-portfolio/static/media/Kate_Bonner_CV.821d293c.pdf"
+  },
+  {
+    "revision": "04d47c40746950d852407f7eeeb2bb06",
+    "url": "/react-portfolio/static/media/katie-04.04d47c40.png"
+  },
+  {
+    "revision": "8d8dd8781be09161b924cf234657b551",
+    "url": "/react-portfolio/index.html"
+  }
+];
