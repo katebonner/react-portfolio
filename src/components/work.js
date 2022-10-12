fe576bb9e5afbea5ee05b3733e825de005d65446
Kate Bonner <katebonner277@gmail.com>
2022-10-12 12:58:07 -0400

    updated url

--- src/components/work.js
+++ src/components/work.js
@@ -6,7 +6,7 @@ function Work() {
         title: 'LEVEL',
         demoPath: 'level.mov', 
         date: 'OCTOBER 2022',
-        deployedApplication: 'https://damp-sea-22160.herokuapp.com/',
+        deployedApplication: 'https://boiling-retreat-31003.herokuapp.com/',
         github: 'https://github.com/katebonner/level',
         description: "CREATED FOR USERS WHO WANT TO FIND OUT THE HAPPINESS LEVEL ASSOCIATED WITH THEIR TOP TRACKS ON SPOTIFY",
         technologies: ['SPOTIFY API', 'REACT', 'REACT QUERY', 'JAVASCRIPT', 'CSS']
